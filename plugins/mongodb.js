d49a3067b448820593f0f14de7cb402a2bc8960e
seongahpark <sksmsekekek21@naver.com>
2022-04-01 23:52:46 +0900

    [fix] plugins/mongodb.js mongodburl edit mongodb url 주소가 잘못돼 수정해줌

--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -3,11 +3,11 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(async function (fastify, opts) {
-//   if (process.env.NODE_ENV === 'test') {
-//     return
-//   }
-//   fastify.register(require('fastify-mongodb'), {
-//     forceClose: true,
-//     url: 'mongodb://root:example@p2-lb-40d4648e2d42ac79.elb.ap-northeast-2.amazonaws.com:27017/baedal'
-//   })
+  if (process.env.NODE_ENV === 'test') {
+    return
+  }
+  fastify.register(require('fastify-mongodb'), {
+    forceClose: true,
+    url: 'mongodb://root:example@p2-lb-40d4648e2d42ac79.elb.ap-northeast-2.amazonaws.com:27017/'
+  })
 })
